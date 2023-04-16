0ad12aa3e30dcb0c0b9dbc100b6e17d751c493bf
pongchul <inchul7511@gmail.com>
2023-04-16 14:25:23 +0900

    Spring Transaction Propagation Example

--- src/main/java/hello/springtx/propagation/MemberService.java
+++ src/main/java/hello/springtx/propagation/MemberService.java
@@ -3,6 +3,8 @@ package hello.springtx.propagation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -12,6 +14,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final LogRepository logRepository;
 
+    @Transactional
     public void joinV1(String username) {
         Member member = new Member(username);
         Log logMessage = new Log(username);
